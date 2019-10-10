4f2b8d40e3ad73d0ca5fa15667bd20399e17bb27
rmaghsoudi <romy_mag@outlook.com>
2019-10-10 12:06:06 -0400

    added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   root to: "users#index"
-
+  resources :entries
+  resources :exercises
+  resources :tests
   devise_for :users,
     path: '',
     path_names: {
